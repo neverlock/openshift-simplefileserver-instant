f4978456e8742cecf67f45b3eed1104adabed59b
neverlock <neverlock@gmail.com>
2015-03-20 20:56:19 +0700

    init

--- web.go
+++ web.go
@@ -1,22 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"os"
-	"runtime"
+  "os"
+  "fmt"
+  "net/http"
 )
 
 func main() {
-	http.HandleFunc("/", hello)
-	bind := fmt.Sprintf("%s:%s", os.Getenv("HOST"), os.Getenv("PORT"))
-	fmt.Printf("listening on %s...", bind)
-	err := http.ListenAndServe(bind, nil)
-	if err != nil {
-		panic(err)
-	}
-}
 
-func hello(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(res, "hello, world from %s", runtime.Version())
+  fs := http.FileServer(http.Dir("./static"))
+  http.Handle("/", fs)
+  bind :=fmt.Sprintf("%s:%s", os.Getenv("HOST"), os.Getenv("PORT"))
+  fmt.Printf("listening on %s...", bind)
+  http.ListenAndServe(bind, nil)
+
 }
